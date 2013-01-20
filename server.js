c2eb87091ced4e19e97d4eab6b73dad0f6c278f4
Sreejesh Karunakaran <sree@tech-avishkar.in>
2013-01-20 22:11:23 +0530

    updated

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 var http = require('http');
 var sys = require('sys');
 var exec = require('child_process').exec;
+var message;
 
 function fortuneCallback(error, stdout, stderr) { 
-    return stderr;       
+    message = stdout;       
 }
 
 var server = http.createServer(function(req, resp){
-    // Get a new string from fortune
-    var message = exec("fortune", fortuneCallback);
+     //Get a new string from fortune
+    exec("fortune", fortuneCallback);
     
     resp.writeHead(200, { "Content-Type": "text/plain"});
     resp.end( message + "\n");
 });
+server.listen(8080);
