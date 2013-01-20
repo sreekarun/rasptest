bec7656ae472da469b513a656c033e8ccda43db3
Sreejesh Karunakaran <sree@tech-avishkar.in>
2013-01-20 21:58:26 +0530

    updated for fortune stuff

--- server.js
+++ server.js
@@ -2,7 +2,14 @@ var http = require('http');
 var sys = require('sys');
 var exec = require('child_process').exec;
 
+function fortuneCallback(error, stdout, stderr) { 
+    return stderr;       
+}
+
 var server = http.createServer(function(req, resp){
+    // Get a new string from fortune
+    var message = exec("fortune", fortuneCallback);
+    
     resp.writeHead(200, { "Content-Type": "text/plain"});
-    resp.end("Hello world\n");
+    resp.end( message + "\n");
 });
